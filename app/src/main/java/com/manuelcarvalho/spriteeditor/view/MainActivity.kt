27c17a072ec25e6350aafa2582d29554942ff876
AndroidOS <burrowsgit@gmail.com>
2020-02-01 07:40:33 +1100

    Added Character fragment

--- app/src/main/java/com/manuelcarvalho/spriteeditor/view/MainActivity.kt
+++ app/src/main/java/com/manuelcarvalho/spriteeditor/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.manuelcarvalho.spriteeditor
+package com.manuelcarvalho.spriteeditor.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.manuelcarvalho.spriteeditor.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
